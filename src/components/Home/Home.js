57d6e3ac3a535263b56e8651a2ae373f2c3d7857
prathibhapenta <prathibhapenta98@gmail.com>
2025-09-17 10:30:36 +0530

    first commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -65,33 +65,33 @@ const Home = () => {
       ],
     },
      attendance: {
-    title: "Attendance",
-    items: [
-      {
-        id: "at1",
-        progress: "79.7%",
-        total: 133,
-        present: 106,
-        absent: 27,
-        courseduration: "Java - July (24)"
-      },
-      {
-        id: "at2",
-        progress: "70.5%",
-        total: 44,
-        present: 31,
-        absent: 13,
-        courseduration: "Python"
-      },
-      {
-        id: "at3",
-        progress: "85%",
-        total: 51,
-        present: 41,
-        absent: 10,
-        courseduration: "Mastering FrontEnd- Technologies"
-      },
-    ],
+      title: "Attendance",
+      items: [
+        {
+          id: "at1",
+          progress: "79.7%",
+          total: 133,
+          present: 106,
+          absent: 27,
+          courseduration: "Java - July (24)"
+        },
+        {
+          id: "at2",
+          progress: "70.5%",
+          total: 44,
+          present: 31,
+          absent: 13,
+          courseduration: "Python"
+        },
+        {
+          id: "at3",
+          progress: "85%",
+          total: 51,
+          present: 41,
+          absent: 10,
+          courseduration: "Mastering FrontEnd- Technologies"
+        },
+      ],
     },
 
 
@@ -143,16 +143,26 @@ const Home = () => {
           feedback:
             "I am glad to share that I got placed through One Solutions with a package of 3.5 LPA. The constant support, mock interviews, and practical training provided by the institute helped me gain confidence and improve my skills.",
         },
-        {
-          id: "pl2",
-          name: "Ravi Kumar",
-          role: "Software Engineer @ Infosys",
-          batch: "Full Stack Dev . Aug 2022",
-          college: "VCET",
-          company: "Infosys",
-          package: "4.2 LPA",
+         {
+          id: "pl1",
+          name: "Shama Shayed",
+          role: "AI Developer @ DigiTod Technologies",
+          batch: "One Solutions . June 2022",
+          college: "Government Engineering College",
+          company: "Wipro",
+          package: "3.5 LPA",
           feedback:
-            "The training and hands-on projects really boosted my confidence and helped me land my dream job.",
+            "I am glad to share that I got placed through One Solutions with a package of 3.5 LPA. The constant support, mock interviews, and practical training provided by the institute helped me gain confidence and improve my skills.",
+        }, {
+          id: "pl1",
+          name: "Shama Shayed",
+          role: "AI Developer @ DigiTod Technologies",
+          batch: "One Solutions . June 2022",
+          college: "Government Engineering College",
+          company: "Wipro",
+          package: "3.5 LPA",
+          feedback:
+            "I am glad to share that I got placed through One Solutions with a package of 3.5 LPA. The constant support, mock interviews, and practical training provided by the institute helped me gain confidence and improve my skills.",
         },
       ],
     },
@@ -270,7 +280,7 @@ const Home = () => {
           style={{ marginRight: "8px", padding: "10px" }}></i></h1>
           <div className="live">
               {homeData.practice.items.map((item) => (
-                <div key={item.id} className="liveclasses-container" style={{ height: "30vh" }}>
+                <div key={item.id} className="liveclasses-container card" >
                   <div className="information">
                     <div className="class-info"
                       style={{ backgroundColor: item.color, minHeight: "80px" }}>
@@ -330,11 +340,10 @@ const Home = () => {
           </div>
 
           <h1>{homeData.attendance.title}<i className="bi bi-question-circle"
-            style={{ marginRight: "8px", padding: "10px" }}></i>
-            </h1>
+            style={{ marginRight: "8px", padding: "10px" }}></i> </h1>
             <div className="live">
         {homeData.attendance.items.map((item) => (
-          <div key={item.id} className="liveclasses-container" style={{ height: "29vh" }}>
+          <div key={item.id} className="liveclasses-container card">
             <div className="information">
               <div className="progress-time">
                     <div className="row">
@@ -385,131 +394,56 @@ const Home = () => {
         ))}
           </div>
 
-        <h1>Placements Achievements<i className="bi bi-question-circle" style={{ marginRight: "8px", padding: "10px"  }}></i></h1>
-          <div className='live' style={{ height: "45vh" }}>
-         <div className='liveclasses-container'>
-        <div className='information'>
-          <div className='class-info' style={{marginBottom: "5px",}}>
-            <img src="/assets/placements.jpg" alt="BroKod" className='placementimg'/>
-            <div className='class-text'>
-              <h3>Shama Shayed</h3>
-              <p>AI Developer @ DigiTod Technologies</p>
-              <p>One Solutions . June 2022</p>
+          <h1>{homeData.placements.title}<i className="bi bi-question-circle"
+            style={{ marginRight: "8px", padding: "10px" }}></i>
+        </h1>
+        <div className="live" style={{ height: "45vh" }}>
+          {homeData.placements.items.map((item) => (
+            <div key={item.id} className="liveclasses-container" style={{ paddingTop: "20px" }}>
+              <div className="information">
+                <div className="class-info" style={{ marginBottom: "5px" }}>
+                  <img src="/assets/placements.jpg" alt={item.name} className="placementimg" />
+                  <div className="class-text">
+                    <h3>{item.name}</h3>
+                    <p>{item.role}</p>
+                    <p>{item.batch}</p>
+                  </div>
+                </div>
+                <hr/>
+                <div className="progress-time" style={{ marginTop: "0" }}>
+                  <div className="row">
+                    <span className="span">College</span>
+                    <p>{item.college}</p>
+                  </div>
+                  <div
+                    className="placed-container"
+                    style={{
+                      background: "linear-gradient(to right, #e3f2fd, #e8f5e9)",
+                      boxShadow: "0 2px 6px rgba(0, 0, 0, 0.15)",display: "flex",
+                      flexDirection: "row",alignItems: "center",justifyContent: "space-between",
+                      padding: "6px 12px", borderRadius: "6px",marginBottom: "0",
+                    }}>
+                    <p style={{ margin: 0, fontSize: "14px" }}>
+                      Got Placed in {item.company}
+                    </p>
+                    <h4 style={{ margin: 0, fontSize: "16px" }}>{item.package}</h4>
+                  </div>
+                </div>
+                <div className="actions"
+                  style={{
+                    display: "flex",justifyContent: "flex-start",
+                    alignItems: "flex-start",padding: "10px 15px",
+                    backgroundColor: "transparent",marginTop: "0",
+                  }}>
+                  <p style={{ fontSize: "14px", fontWeight: "400", lineHeight: "1.6", color: "#333", margin: "0",}}>
+                    {item.feedback}
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-            <hr style={{  border: "none",  borderTop: "1px solid #ccc", width: "90%" }} />
-          <div className='progress-time' style={{marginTop: "0"  }}>
-            <div className='row'>
-              <span className='span'>College</span>
-              <p>Governement Engineering College</p>
-            </div>
-            <div 
-              className="placed-container"
-              style={{
-                background: "linear-gradient(to right, #e3f2fd, #e8f5e9)",boxShadow: "0 2px 6px rgba(0, 0, 0, 0.15)",
-                display: "flex",flexDirection: "row",alignItems: "center",
-                justifyContent: "space-between", padding: "6px 12px",borderRadius: "6px",marginBottom: "0"  
-              }}>
-              <p style={{ margin: 0, fontSize: "14px" }}>Got Placted in Wipro</p>
-              <h4 style={{ margin: 0, fontSize: "16px" }}>3.5 LPA</h4>
-            </div>
-          </div>
-          <div  className='actions' style={{ display: "flex",justifyContent: "flex-start",alignItems: "flex-start",
-              padding: "10px 15px",
-              backgroundColor: "transparent", 
-              marginTop: "0"  
-            }}>
-            <p style={{ fontSize: "14px",fontWeight: "400",lineHeight: "1.6",color: "#333",margin: "0"}} >
-              I am glad to share that I got placed through One Solutions with a package of 3.5 LPA. 
-              The constant support, mock interviews, and practical training provided by the institute 
-              helped me gain confidence and improve my skills.
-            </p>
-          </div>
+          ))}
         </div>
-      </div>
-      <div className='liveclasses-container'>
-        <div className='information'>
-          <div className='class-info' style={{marginBottom: "5px",}}>
-            <img src="/assets/placements.jpg" alt="BroKod" className='placementimg'/>
-            <div className='class-text'>
-              <h3>Shama Shayed</h3>
-              <p>AI Developer @ DigiTod Technologies</p>
-              <p>One Solutions . June 2022</p>
-            </div>
-          </div>
-            <hr style={{  border: "none",  borderTop: "1px solid #ccc", width: "90%" }} />
-          <div className='progress-time' style={{marginTop: "0"  }}>
-            <div className='row'>
-              <span className='span'>College</span>
-              <p>Governement Engineering College</p>
-            </div>
-            <div 
-              className="placed-container"
-              style={{
-                background: "linear-gradient(to right, #e3f2fd, #e8f5e9)",boxShadow: "0 2px 6px rgba(0, 0, 0, 0.15)",
-                display: "flex",flexDirection: "row",alignItems: "center",
-                justifyContent: "space-between", padding: "6px 12px",borderRadius: "6px",marginBottom: "0"  
-              }}>
-              <p style={{ margin: 0, fontSize: "14px" }}>Got Placted in Wipro</p>
-              <h4 style={{ margin: 0, fontSize: "16px" }}>3.5 LPA</h4>
-            </div>
-          </div>
-          <div className='actions' style={{ display: "flex",justifyContent: "flex-start",alignItems: "flex-start",
-              padding: "10px 15px",
-              backgroundColor: "transparent", 
-              marginTop: "0"  
-            }}>
-            <p style={{ fontSize: "14px",fontWeight: "400",lineHeight: "1.6",color: "#333",margin: "0"}} >
-              I am glad to share that I got placed through One Solutions with a package of 3.5 LPA. 
-              The constant support, mock interviews, and practical training provided by the institute 
-              helped me gain confidence and improve my skills.
-            </p>
-          </div>
         </div>
-      </div>
-      <div className='liveclasses-container'>
-        <div className='information'>
-          <div className='class-info' style={{marginBottom: "5px",}}>
-            <img src="/assets/placements.jpg" alt="BroKod" className='placementimg'/>
-            <div className='class-text'>
-              <h3>Shama Shayed</h3>
-              <p>AI Developer @ DigiTod Technologies</p>
-              <p>One Solutions . June 2022</p>
-            </div>
-          </div>
-            <hr style={{  border: "none",  borderTop: "1px solid #ccc", width: "90%" }} />
-          <div className='progress-time' style={{marginTop: "0"  }}>
-            <div className='row'>
-              <span className='span'>College</span>
-              <p>Governement Engineering College</p>
-            </div>
-            <div 
-              className="placed-container"
-              style={{
-                background: "linear-gradient(to right, #e3f2fd, #e8f5e9)",boxShadow: "0 2px 6px rgba(0, 0, 0, 0.15)",
-                display: "flex",flexDirection: "row",alignItems: "center",
-                justifyContent: "space-between", padding: "6px 12px",borderRadius: "6px",marginBottom: "0"  
-              }}>
-              <p style={{ margin: 0, fontSize: "14px" }}>Got Placted in Wipro</p>
-              <h4 style={{ margin: 0, fontSize: "16px" }}>3.5 LPA</h4>
-            </div>
-          </div>
-           <div className='actions' style={{ display: "flex",justifyContent: "flex-start",alignItems: "flex-start",
-              padding: "10px 15px",
-              backgroundColor: "transparent", 
-              marginTop: "0"  
-            }}>
-            <p style={{ fontSize: "14px",fontWeight: "400",lineHeight: "1.6",color: "#333",margin: "0"}} >
-              I am glad to share that I got placed through One Solutions with a package of 3.5 LPA. 
-              The constant support, mock interviews, and practical training provided by the institute 
-              helped me gain confidence and improve my skills.
-            </p>
-          </div>
-        </div>
-      </div>
-      </div>
-      </div>
   )
 }
-
 export default Home
